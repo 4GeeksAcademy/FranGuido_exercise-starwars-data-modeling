acad9110bcd9a1253de8be094d49af03ea37fb86
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-08-05 22:55:31 +0000

    Finished

--- src/starwars.py
+++ src/starwars.py
@@ -10,7 +10,49 @@ Base = declarative_base()
 class Characters(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key = True)
-    name = Column(String(50), nullable=False)
+    name = Column(String(50))
+    birth_date = Column(String(10))
+    height = Column(String(10))
+    hair_color = Column(String(10))
+    homeworld = Column(Integer, ForeignKey('planets.id'))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(50))
+    population = Column(String(50))
+    diameter = Column(String(50))
+    characters = Column(Integer, ForeignKey('characters.id'))
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(50))
+    model = Column(String(50))
+    manufacturer = Column(String(50))
+
+class FavoriteCharacters(Base):
+    __tablename__ = 'favoritecharacters'
+    id = Column(Integer, primary_key = True)
+    id_character = Column(Integer, ForeignKey('characters.id'))
+
+class FavoritePlanets(Base):
+    __tablename__ = 'favoriteplanets'
+    id = Column(Integer, primary_key = True)
+    id_planet = Column(Integer, ForeignKey('planets.id'))
+
+class FavoriteVehicles(Base):
+    __tablename__ = 'favoritevehicles'
+    id = Column(Integer, primary_key = True)
+    id_vehicle = Column(Integer, ForeignKey('vehicles.id'))
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key = True)
+    fav_chars = Column(Integer, ForeignKey('favoritecharacters.id'))
+    fav_planets = Column(Integer, ForeignKey('favoriteplanets.id'))
+    fav_vehicles = Column(Integer, ForeignKey('favoritevehicles.id'))
+    
 
     def to_dict(self):
         return {}
